aa934151b43cc0341a7a486681aa951e5fac308c
Dan Stewart <danielandrewstewart@gmail.com>
2017-10-01 20:16:27 +0100

    Initial commit

---
+++ bf.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use lib 'lib';
+use BFI qw/execute displaycells resetcells/;
+use Term::ReadKey;
+use List::MoreUtils qw(any);
+
+# TODO: the displaycells sub probably belongs
+# here rather than in the BFI package.
+# The BFI package should only contain brainfuck
+# processing logic.
+
+sub getkey {
+	ReadMode 4;
+	my $key;
+	until (defined ($key = ReadKey(-1))) { }
+	return $key;
+}
+
+sub getline {
+	ReadMode 0;
+	my $line;
+	while (not defined ($line = ReadLine(0))) { }
+	chomp $line;
+	return $line;
+}
+
+sub repl {
+	# TODO: Tidy to avoid having to print before loop and in loop
+	# TODO: Tidy to avoid having to next if key is Q
+	# TODO: Add a marker for the current cell (Will be a change to displaycells)
+	my $key = "";
+	my $keyhistory = "";
+	my @validkeys = ('+', '-', '>', '<', '.', ',');
+
+	print "REPL Mode\n";
+	print "=========\n\n";
+	print displaycells() . "\n\n";
+	while ($key ne 'Q' and $key ne 'q') {
+		$key = getkey();
+		next if $key eq "Q" or $key eq "q";
+
+		execute($key) if any { $key eq $_ } @validkeys;
+		system 'clear';
+		print "REPL Mode\n";	
+		print "=========\n\n";
+		print displaycells() . "\n\n";
+
+		print $keyhistory .= $key;
+	}
+}
+
+print "Hello! Welcome to the BrainFuck Interpretor!\n";
+print "To execute some code just hit E, or press ? for help.\nPress Q to quit.\n";
+my $key = "";
+while ($key ne 'Q' and $key ne 'q') {
+	$key = getkey();
+
+	if ($key eq 'E' or $key eq 'e') {
+		# TODO: Allow line breaks
+		#       Maybe accept input after 1 or 2 blank lines
+		print "Please enter your code: \n";
+		my $line = getline();
+		my $result = execute($line);
+		print "Output: " . $result . "\n" if $result;
+	} elsif ($key eq 'D' or $key eq 'd') {
+		print "\n" . displaycells() . "\n";
+	} elsif ($key eq 'R' or $key eq 'r') {
+		resetcells();
+	} elsif ($key eq '?') {
+		print "Help Page\n";
+		print "=========\n";
+		print "E: Execute some code\n";
+		print "D: Display cells\n";
+		print "C: Clear the screen\n";
+		print "R: Reset the cells\n";
+		print "P: Enter REPL mode\n";
+		print "Q: Quit\n";
+		print "?: Show help\n";
+	} elsif ($key eq 'C' or $key eq 'c') {
+		system 'clear';
+		print "Hello! Welcome to the BrainFuck Interpretor!\n";
+		print "To execute some code just hit E, or press ? for help. Press Q to quit.\n";
+	} elsif ($key eq "P" or $key eq "p") {
+		system 'clear';
+		repl();
+		print "\nExited REPL - Press 'Q' again to exit\n";
+	}
+}
+
+ReadMode 0;
---
+++ lib/BFI.pm
@@ -0,0 +1,127 @@
+#!/usr/bin/perl
+
+package BFI;
+
+use strict;
+use warnings;
+use List::Util qw/max/;
+use List::MoreUtils qw/any/;
+
+use Exporter qw/import/;
+our @EXPORT_OK = qw/execute displaycells resetcells/;
+
+# INSTRUCTIONS
+#-------------------------------
+# + : increment current cell
+# - : decrement current cell
+# , : read STDIN into cell
+# . : print cell to STDOUT
+# > : shift to next cell
+# < : shift to previous cell
+# [ : jump to ']' if current cell is 0, else go to next command
+# ] : jump back to '[' if current cell is greater than 0, else go to next command
+
+my %cells;
+$cells{$_} = 0 for (0..10);
+my $pos = 0;
+my $output = "";
+
+sub in {
+	print "Enter input: ";
+	my $input = <STDIN>;
+	chomp $input;
+	$cells{$pos} = ord($input);
+}
+
+my %symbolmap = (
+  '+' => sub { $cells{$pos}++ },
+	'-' => sub { $cells{$pos}-- if $cells{$pos} },
+	'>' => sub { $pos++; $cells{$pos} = 0 if not defined $cells{$pos} },
+	'<' => sub { $pos-- if $pos },
+	'.' => sub { $output .= chr $cells{$pos} },
+	',' => \&in
+);
+
+sub execute {
+	my $code = shift;
+	my @commands = split //, $code;
+	my $commandindex = 0;
+	my @markers;
+	my $skiploop;
+	$output = "";
+
+	while ($commandindex <= $#commands) {
+		my $command = $commands[$commandindex];
+
+	  if ($skiploop){
+			$skiploop = 0 if $command eq ']';
+			next;
+	  }
+
+	  if (any { $_ eq $command } keys %symbolmap){
+		  $symbolmap{$command}->();
+	  } elsif ($command eq '[') {
+			if ($cells{$pos}){
+				push @markers, $commandindex;
+			} else {
+				$skiploop = 1;
+			}
+	  } elsif ($command eq ']') {
+			if ($cells{$pos}) {
+				# Jump back to start of loop
+				$commandindex = pop @markers;
+				next;
+			}
+		}
+
+		$commandindex++;
+  }
+
+	return $output if $output;
+}
+
+sub resetcells {
+	undef %cells;
+	$cells{$_} = 0 for (0..10);
+}
+
+sub pad {
+	# TODO: Would be cool to centre or left align
+	my ($input, $fill, $width) = @_;
+	return $input . ($fill x ($width - length($input)));
+}
+
+sub displaycells {
+	my $indexes = "Cell  |";
+	my $values  = "Value |";
+	my $chars   = "ASCII |";
+	my $breaks  = "------|";
+
+	# This is used to convert chars that don't display correctly
+	# Or mess up the table (like new lines)
+	my %symbols = (
+		0  => "NUL", 1  => "SOH", 2  => "STX", 3  => "ETX", 4 => "EOT",
+		5  => "ENQ", 6  => "ACK", 7  => "BEL", 8  => "BS",  9 => "HT",
+		10 => "LF",  11 => "VT",  12 => "FF",  13 => "CR",  14 => "SO",
+		15 => "SI",  16 => "DLE", 17 => "DC1", 18 => "DC2", 19 => "DC3",
+		20 => "DC4", 21 => "NAK", 22 => "SYN", 23 => "ETB", 24 => "CAN",
+		25 => "EM",  26 => "SUB", 27 => "ESC", 28 => "FS",  29 => "GS",
+		30 => "RS",  31 => "US"
+	);
+
+	foreach my $cell (sort {$a <=> $b } keys %cells) {
+		my $value = $cells{$cell};
+		my $ascii = $value < 32 ? $symbols{$value} : chr($cells{$cell});
+
+		my $width = max(length $value, length $cell, length $ascii);
+
+		$indexes .= pad($cell,  " ", $width) . "|";
+		$values  .= pad($value, " ", $width) . "|";
+		$chars   .= pad($ascii, " ", $width) . "|";
+		$breaks  .= pad("",     "-", $width) . "|";
+	}
+
+  return join "\n", ($indexes, $breaks, $values, $breaks, $chars);
+}
+
+1;
